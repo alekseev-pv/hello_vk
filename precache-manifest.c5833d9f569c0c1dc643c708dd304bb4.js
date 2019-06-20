d92d5ca9ac80c222f4ebbff188d991a7adb718aa
Pavel Alekseev <aleks2071@ya.ru>
2019-06-20 15:15:58 +0300

    build

---
+++ precache-manifest.c5833d9f569c0c1dc643c708dd304bb4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/hello_vk/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "f5bfd8690273b8997fb1",
+    "url": "/hello_vk/static/js/runtime~main.f5bfd869.js"
+  },
+  {
+    "revision": "3df86ad4c789cd6499e2",
+    "url": "/hello_vk/static/js/main.3df86ad4.chunk.js"
+  },
+  {
+    "revision": "5fb28dc85f68753c7895",
+    "url": "/hello_vk/static/js/2.5fb28dc8.chunk.js"
+  },
+  {
+    "revision": "3df86ad4c789cd6499e2",
+    "url": "/hello_vk/static/css/main.a2270c4c.chunk.css"
+  },
+  {
+    "revision": "5fb28dc85f68753c7895",
+    "url": "/hello_vk/static/css/2.532ba40f.chunk.css"
+  },
+  {
+    "revision": "362424c1fe16f6ebef7ab40a142716db",
+    "url": "/hello_vk/index.html"
+  }
+];
